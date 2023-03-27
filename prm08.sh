4f6288013971229f6f8a652963f391949324f6d4
REM-moe <98999089+REM-moe@users.noreply.github.com>
2023-03-27 10:04:58 +0530

    Delete prm08.sh

--- prm08.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Enter Number To Check Odd Or Even: "$'\n' number
-if [ `expr $number % 2` == 0 ]
-then 
-echo "Entered Number : $number Is EVEN!!"
-else
-echo "Entered Number : $number Is ODD!!"
-fi
