45be36b0b57227f414fa8291e98b18123dd11efd
REM-moe <98999089+REM-moe@users.noreply.github.com>
2023-04-03 09:52:30 +0530

    Add files via upload

---
+++ Inter_Process_Communications/Read_inter-Process-Communication.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include<string.h>
+
+int main(){
+	int id = shmget(1328, 1024, 0666); // a key(any int.), memory space, flag bit, (if shared seg dosent exist we use IPC_CREAT)
+	printf("Shared Memeory id: %d \n", id);
+	char *addr_SM = shmat(id, NULL, 0); 	/* [attach]The third parameter is ‘0’ 
+											   if the second parameter is NULL, otherwise, 
+											   the value specified by SHM_RND. */
+	printf("Process is attached at Address: %p\n", addr_SM);
+	printf("Data Read From Shared Memory!! %s\n", addr_SM);
+	return 0;
+}
