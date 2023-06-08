9028151b3627cade19baab42c36f6f7c9a5181fb
REM-moe <98999089+REM-moe@users.noreply.github.com>
2023-06-08 15:05:47 +0530

    Update Priority_scheduling.c

--- Process Scheduling/Priority_scheduling.c
+++ Process Scheduling/Priority_scheduling.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 struct Process {
   int p_id ;
   int priority;
@@ -9,26 +8,14 @@ struct Process {
   int TAT;
   int waiting_time;
 };
-
 void main() {
   printf("Enter Number Of Process: ");
   int pnum = 0;
-  scanf("%d",&pnum);
-  
+  scanf("%d",&pnum); 
   struct Process process[pnum];
-
   for(int i =0; i<pnum; i++) {
-    process[i].p_id = 0;
- 	process[i].priority = 0;
- 	process[i].arrival_time= 0;
- 	process[i].burst_time = 0;
- 	process[i].completion_time  = 0;
- 	process[i].TAT = 0;
- 	process[i].waiting_time = 0;
- 	
-	process[i].p_id += i+1;
- 	
- 	
+    process[i] = {0};
+    process[i].p_id += i+1;	
     printf("Enter arrival Time for process number [ %d ] ", process[i].p_id);
     scanf("%d",&process[i].arrival_time);
     printf("Enter Burst Time For Process number [ %d ] ", process[i].p_id);
@@ -36,32 +23,17 @@ void main() {
     printf("Enter Priority Of Process number [ %d ] ", process[i].p_id);
     scanf("%d",&process[i].priority);
   }
-
-  // Since We are Using First Come First Serve We Sort the Struct array based on Arrival Time; 
   //Bubble Sort is fine
   for(int j =0; j<pnum; j++) {
     for (int k = 0; k<pnum-j ;k++) {
       if (process[k].arrival_time > process[k+1].arrival_time) {
-        struct Process temp[pnum];
-        
-        
-	temp[0].p_id = 0;
- 	temp[0].priority = 0;
- 	temp[0].arrival_time= 0;
- 	temp[0].burst_time = 0;
- 	temp[0].completion_time  = 0;
- 	temp[0].TAT = 0;
- 	temp[0].waiting_time = 0;
-        
-        
-        
+        struct Process temp[pnum] = {0};        
         temp[0] = process[k];
         process[k] = process[k+1];
         process[k+1] = temp[0];
       }
     }
   }
-  
   // Next Sort Using Priority (Higher Number = Higher Priority)
 	for(int j=1;j<pnum-1;j++){
 		for(int k=1; k<pnum-j; k++){
@@ -74,22 +46,15 @@ void main() {
 			}
 		}
 	}
-
   // Calculation
   int sum = 0; 
   float AverageTAT, AverageWT, TotalTAT, TotalWT = 0.0;
-
   for(int i =0; i<pnum; i++) {
-    sum = sum + process[i].burst_time;
-      
-    process[i].completion_time = process[i].completion_time + sum;
-      
+    sum = sum + process[i].burst_time;   
+    process[i].completion_time = process[i].completion_time + sum; 
     process[i].TAT = process[i].completion_time - process[i].arrival_time;
-    
     process[i].waiting_time = process[i].TAT - process[i].burst_time;
-
     TotalTAT += process[i].TAT;
-
     TotalWT += process[i].waiting_time;
     }
 	  printf("\n ***** PRIORITY SCHEDULING ***** \n ");
